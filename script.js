2cf462aae54135d4ed5051f29c1a78c51d9e0100
Jesica Biecher <j.biecher@fvfj92d21wgc.speedport.ip>
2022-12-15 13:35:05 +0100

    final

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let operator = null;
 let firstOperator = null;
 let secondOperator = null;
 
-const displayEl = document.querySelector('.displayNumber');
-const displayNew = document.querySelector('.displayResult');
+const displayNum = document.querySelector('.displayNumber');
+const displayRes = document.querySelector('.displayResult');
 const numbers = document.querySelectorAll('.numbers');
 const decimal = document.querySelector('.dot');
 const operators = document.querySelectorAll('.operators');
@@ -34,7 +34,7 @@ delButton.addEventListener('click', () => {
 });
 
 equalButton.addEventListener('click', () => {
-    inputEqual();
+    inputEqual(equalButton.textContent);
 });
 
 operators.forEach((operator) => {
@@ -45,59 +45,74 @@ operators.forEach((operator) => {
 
 //FUNCTIONS
 function inputNumber(number) {
-    displayEl.textContent = displayEl.textContent === '0' ? number : displayEl.textContent + number;
+    displayNum.textContent = displayNum.textContent === '0' ? number : displayNum.textContent + number;
 };
 
 function inputDecimal(dot) {
-    if (!displayEl.textContent.includes(dot)) {
-        displayEl.textContent += dot;
+    if (!displayNum.textContent.includes(dot)) {
+        displayNum.textContent += dot;
     }
 };
 
 function clear() {
-    displayEl.textContent = 0;
-    displayNew.textContent = 0;
+    displayRes.hidden = false;
+    displayNum.hidden = false;
+    displayNum.textContent = 0;
+    displayRes.textContent = 0;
     firstNumber = '';
     secondNumber = '';
     operator = null;
 };
 
 function del() {
-    if (displayEl.textContent == 0 || displayEl.textContent.length == 1) {
-        displayEl.textContent = 0;
+    if (displayNum.textContent == 0 || displayNum.textContent.length == 1) {
+        displayNum.textContent = 0;
     } else {
-        displayEl.textContent = displayEl.textContent
+        displayNum.textContent = displayNum.textContent
             .toString()
             .slice(0, -1)
     }
 };
 
-function inputEqual() {
-    inputOperator();
+function inputEqual(equalButton) {
+    inputOperator(equalButton);
 };
 
 function inputOperator(operator) {
     if (firstNumber == '' && firstOperator != '') {
-        firstNumber = parseFloat(displayEl.textContent);
-        displayNew.textContent = firstNumber;
+        firstNumber = parseFloat(displayNum.textContent);
+        displayRes.textContent = firstNumber;
         firstOperator = operator;
 
-        displayEl.textContent = 0;
+        displayNum.textContent = 0;
 
-    }else if (firstNumber != '' && secondNumber == '') {
-        secondNumber = parseFloat(displayEl.textContent);
-        displayNew.textContent = firstNumber;
+    } else if (firstNumber != '' && secondNumber == '') {
+        secondNumber = parseFloat(displayNum.textContent);
+        displayRes.textContent = firstNumber;
         secondOperator = operator;
 
         const result = operate(firstOperator, firstNumber, secondNumber);
 
-        displayEl.textContent = 0;
-        displayNew.textContent = result;
-        firstNumber = result;
-        firstOperator = secondOperator;
-        secondNumber = '';
-        secondOperator = null;
-    } 
+        if (operator == '=') {
+            displayRes.hidden = false;
+            displayNum.hidden = true;
+            displayNum.textContent = result;
+            displayRes.textContent = result;
+            firstNumber = result;
+            firstOperator = secondOperator;
+            secondNumber = '';
+            secondOperator = null;
+        } else {
+            displayRes.hidden = false;
+            displayNum.hidden = false;
+            displayNum.textContent = 0;
+            displayRes.textContent = result;
+            firstNumber = result;
+            firstOperator = secondOperator;
+            secondNumber = '';
+            secondOperator = null;
+        }
+    }
 };
 
 //FUNCTIONS
@@ -117,8 +132,13 @@ function multiply(firstNumber, secondNumber) {
 }
 
 function divide(firstNumber, secondNumber) {
-    result = firstNumber / secondNumber;
-    return result;
+    if (secondNumber === 0) {
+        result = 'oops, try again!';
+        return result;
+    } else {
+        result = firstNumber / secondNumber;
+        return result;
+    }
 };
 
 function power(firstNumber, secondNumber) {
